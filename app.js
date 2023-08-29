9e04a90155733f47bdfc857e0c4eca9c6b99aca3
Daniel Tejada <dastte93@gmail.com>
2023-08-29 12:53:37 -0500

    Probando contenido estatico

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 import express from "express";
 
+import * as url from 'url';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+
 const app = express();
 const port = 8080
 
+// Servir contenido estático
+app.use( express.static('public') )
+
+
 app.get('/', (req, res) => {
-    res.send('Hola mundo')
+    res.send('Home Page')
+});
+
+app.get('/hola-mundo', (req, res) => {
+    res.send('Hola mundo en su respectiva ruta')
 });
 
 app.get('*', (req, res) => {
-    res.send('404 | Page not found')
+    res.sendFile( __dirname + '/public/404.html' )
 });
 
 app.listen(port, () => {
